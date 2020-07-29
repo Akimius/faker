8fc4327d42d33a61b6336b188d222f485bd84e89
Akim <akim@doubledmarketing.com>
2020-07-29 15:16:38 +0300

    first commit

---
+++ app/Http/Controllers/PortingPhonesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\PortingPhones;
+use Illuminate\Http\Request;
+
+class PortingPhonesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\PortingPhones  $portingPhones
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PortingPhones $portingPhones)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\PortingPhones  $portingPhones
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PortingPhones $portingPhones)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\PortingPhones  $portingPhones
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PortingPhones $portingPhones)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\PortingPhones  $portingPhones
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PortingPhones $portingPhones)
+    {
+        //
+    }
+}
---
+++ database/factories/PortingPhonesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\PortingPhones;
+use Faker\Generator as Faker;
+
+$factory->define(PortingPhones::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
