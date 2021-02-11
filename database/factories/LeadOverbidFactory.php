65e7cf24724674f19629691275e12b4029c08be2
Akim <akim@doubledmarketing.com>
2021-02-11 17:41:48 +0200

    lead overbid 3

--- database/factories/LeadOverbidFactory.php
+++ database/factories/LeadOverbidFactory.php
@@ -21,17 +21,50 @@ class LeadOverbidFactory extends Factory
      */
     public function definition(): array
     {
+
         return [
             'creationDatetime' => now(),
-            'leadId'            => 123,
+            'leadId'            => $this->faker->randomElement($this->getLeadIds()),
             'partnerId'         => 0,
-            'campaignId'        => 1,
+            'campaignId'        => $this->faker->randomElement($this->getCampaignIds()),
             'oldBid'            => 123000,
             'newBid'            => 124000,
             'overBid'           => 1000,
             'status'            => $this->faker->randomElement([0, 1, 2]),
             'overBidPartnerId'  => 1,
             'overBidCampaignId' => 2
+        ];
+    }
+
+    private function getCampaignIds(): array
+    {
+        return [
+            1130630,
+            1130631,
+            1130632,
+            1130633,
+        ];
+    }
+
+    private function getLeadIds(): array
+    {
+        return [
+            1130630,
+            1130631,
+            1130632,
+            1130633,
+            1130634,
+            1130635,
+            1130636,
+            1130637,
+            1130638,
+            1130639,
+            1130640,
+            1130641,
+            1130642,
+            1130643,
+            1130644,
+            1131029,
         ];
     }
 }
