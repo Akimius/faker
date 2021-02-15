577dfcf4833969c0d30fc7914d49949e3d07d9fd
Akim <akim@doubledmarketing.com>
2021-02-15 09:26:53 +0200

    lead overbid 4

--- database/factories/LeadOverbidBlackListFactory.php
+++ database/factories/LeadOverbidBlackListFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\LeadOverbidBlackList;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class LeadOverbidBlackListFactory extends Factory
@@ -19,19 +20,46 @@ class LeadOverbidBlackListFactory extends Factory
      * Define the model's default state.
      *
      * @return array
+     * @throws \Exception
      */
     public function definition(): array
     {
+
         return [
             'creationDatetime' => now(),
-            'leadId'            => 123,
-            'partnerId'         => 0,
-            'campaignId'        => 1,
-            'oldBid'            => 123000,
-            'newBid'            => 124000,
-            'overBid'           => 1000,
+            'leadId'            => $this->faker->randomElement($this->getLeadIds()),
+            'partnerId'         => $this->faker->randomElement($this->getPartnerIds()),
+            'campaignId'        => $this->faker->randomElement($this->getCampaignIds()),
+            'oldBid'            => random_int(10, 20),
+            'newBid'            => random_int(20, 30),
+            'overBid'           => random_int(1, 2),
             'status'            => $this->faker->randomElement([0, 1, 2]),
-            'auctionId'         => 1,
+            'auctionId'         => $this->faker->randomElement($this->getPartnerIds()),
         ];
+    }
+
+    private function getPartnerIds(): array
+    {
+        return DB::table('partner')
+            ->take(10)
+            ->pluck('id')
+            ->toArray();
+    }
+
+    private function getCampaignIds(): array
+    {
+        return DB::table('partner_campaign')
+            ->take(10)
+            ->pluck('id')
+            ->toArray();
+    }
+
+    private function getLeadIds(): array
+    {
+        return DB::table('lead_body_paydayus')
+            //->where('name', 'John')
+            ->take(5)
+            ->pluck('id')
+            ->toArray();
     }
 }
--- database/factories/LeadOverbidFactory.php
+++ database/factories/LeadOverbidFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\LeadOverbid;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
 
 class LeadOverbidFactory extends Factory
 {
@@ -18,6 +19,7 @@ class LeadOverbidFactory extends Factory
      * Define the model's default state.
      *
      * @return array
+     * @throws \Exception
      */
     public function definition(): array
     {
@@ -25,46 +27,39 @@ class LeadOverbidFactory extends Factory
         return [
             'creationDatetime' => now(),
             'leadId'            => $this->faker->randomElement($this->getLeadIds()),
-            'partnerId'         => 0,
+            'partnerId'         => $this->faker->randomElement($this->getPartnerIds()),
             'campaignId'        => $this->faker->randomElement($this->getCampaignIds()),
-            'oldBid'            => 123000,
-            'newBid'            => 124000,
-            'overBid'           => 1000,
+            'oldBid'            => random_int(10, 20),
+            'newBid'            => random_int(20, 30),
+            'overBid'           => random_int(1, 2),
             'status'            => $this->faker->randomElement([0, 1, 2]),
-            'overBidPartnerId'  => 1,
-            'overBidCampaignId' => 2
+            'overBidPartnerId'  => $this->faker->randomElement($this->getPartnerIds()),
+            'overBidCampaignId' => $this->faker->randomElement($this->getCampaignIds())
         ];
+    }
+
+    private function getPartnerIds(): array
+    {
+        return DB::table('partner')
+            ->take(10)
+            ->pluck('id')
+            ->toArray();
     }
 
     private function getCampaignIds(): array
     {
-        return [
-            1130630,
-            1130631,
-            1130632,
-            1130633,
-        ];
+        return DB::table('partner_campaign')
+            ->take(10)
+            ->pluck('id')
+            ->toArray();
     }
 
     private function getLeadIds(): array
     {
-        return [
-            1130630,
-            1130631,
-            1130632,
-            1130633,
-            1130634,
-            1130635,
-            1130636,
-            1130637,
-            1130638,
-            1130639,
-            1130640,
-            1130641,
-            1130642,
-            1130643,
-            1130644,
-            1131029,
-        ];
+        return DB::table('lead_body_paydayus')
+            //->where('name', 'John')
+            ->take(5)
+            ->pluck('id')
+            ->toArray();
     }
 }
